404ea7d8c183c6ca0505c3bc70c3f53a4d67d31f
KINGBOTs1 <ds767365@gmail.com>
2023-06-17 04:21:03 +0800

    xx

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/UserController');
+const UserController = require('../controllers/user');
 
 router.post('/login', UserController.login);
 router.post('/register', UserController.register);
