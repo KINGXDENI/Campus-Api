95a0aceac2a639c3973119a22106a5a51d3a7eea
KINGBOTs1 <ds767365@gmail.com>
2023-06-19 13:24:34 +0800

    jj

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -38,7 +38,39 @@ const getLikesByReportId = async (req, res) => {
   }
 };
 
+const removeLike = async (req, res) => {
+  try {
+    const {
+      id
+    } = req.params;
+    // Menemukan report berdasarkan ID
+    const report = await Report.findById(id);
+
+    // Memastikan nilai likes adalah angka
+    if (typeof report.likes !== 'number') {
+      report.likes = 0;
+    }
+
+    // Memastikan jumlah like tidak negatif
+    if (report.likes > 0) {
+      // Mengurangi jumlah like pada report
+      report.likes -= 1;
+      await report.save();
+    }
+
+    res.status(200).json({
+      message: 'Like removed successfully'
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: 'Internal server error'
+    });
+  }
+};
+
 module.exports = {
   addLike,
   getLikesByReportId,
+  removeLike,
 };
--- routes/likeRoute.js
+++ routes/likeRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { addLike, getLikesByReportId } = require('../controllers/likeController.js');
+const { addLike, getLikesByReportId, removeLike } = require('../controllers/likeController.js');
 const router = express.Router();
 
 // Definisikan rute untuk menambahkan like
 router.post('/report/:id/like', addLike);
 router.get('/report/:id/like', getLikesByReportId);
+router.delete('/report/:id/like', removeLike);
 
 module.exports = router;
