089e9b4083a5261dfcdc09ce75f0fa17159f8ead
KINGBOTs1 <ds767365@gmail.com>
2023-08-24 14:14:13 +0800

    new

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -36,6 +36,42 @@ const sendEmail = async (req, res) => {
     }
 };
 
+const sendEmailUser = async (req, res) => {
+    try {
+        const {
+            recipient,
+            subject,
+            message
+        } = req.body;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USERNAME, // Ganti dengan email pengirim
+                pass: process.env.EMAIL_PASSWORD, // Ganti dengan password email pengirim
+            },
+        });
+
+        const mailOptions = {
+            from: 'New Report <' + process.env.EMAIL_USERNAME + '>',
+            to: recipient,
+            subject: subject,
+            html: message,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        res.json({
+            message: 'Email sent successfully.'
+        });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json({
+            error: 'An error occurred while sending the email.'
+        });
+    }
+};
+
 module.exports = {
-    sendEmail
+    sendEmail, sendEmailUser
 };
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {
-    sendEmail
+    sendEmail, sendEmailUser
 } = require('../controllers/emailController');
 const router = express.Router();
 
 // Definisikan rute untuk mengirim email
 router.post('/send-email', sendEmail);
+router.post('/send-email-user', sendEmailUser);
 
 module.exports = router;
