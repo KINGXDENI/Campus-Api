1f3599e46a644a342c4334958996151f1d565abb
KINGBOTs1 <ds767365@gmail.com>
2023-08-24 14:50:07 +0800

    u

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -52,7 +52,42 @@ const sendEmailUser = async (req, res) => {
         });
 
         const mailOptions = {
-            from: 'Info Report <' + process.env.EMAIL_USERNAME + '>',
+            from: 'New Report <' + process.env.EMAIL_USERNAME + '>',
+            to: process.env.EMAIL_USERNAME,
+            subject: subject,
+            html: message,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        res.json({
+            message: 'Email sent successfully.'
+        });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json({
+            error: 'An error occurred while sending the email.'
+        });
+    }
+};
+
+const sendEmailUseru = async (req, res) => {
+    try {
+        const {
+            subject,
+            message
+        } = req.body;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USERNAME, // Ganti dengan email pengirim
+                pass: process.env.EMAIL_PASSWORD, // Ganti dengan password email pengirim
+            },
+        });
+
+        const mailOptions = {
+            from: 'Update Report <' + process.env.EMAIL_USERNAME + '>',
             to: process.env.EMAIL_USERNAME,
             subject: subject,
             html: message,
@@ -72,5 +107,5 @@ const sendEmailUser = async (req, res) => {
 };
 
 module.exports = {
-    sendEmail, sendEmailUser
+    sendEmail, sendEmailUser, sendEmailUseru
 };
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {
-    sendEmail, sendEmailUser
+    sendEmail, sendEmailUser, sendEmailUseru
 } = require('../controllers/emailController');
 const router = express.Router();
 
 // Definisikan rute untuk mengirim email
 router.post('/send-email', sendEmail);
 router.post('/send-email-user', sendEmailUser);
+router.post('/send-email-useru', sendEmailUseru);
 
 module.exports = router;
